016b08b5f15846e29932c800eefec202ec428239
trinityXmontoya <trinityXmontoya@gmail.com>
2016-05-27 19:32:25 -0400

    drug info pg complete

--- client/drugs.js
+++ client/drugs.js
@@ -15,11 +15,11 @@ var info = {
     ],
     successStoriesLink: "http://www.methadone.us/methadone-success-stories/",
     successStories: [
-      {name: "Sharon", image: "women/10.jpg", text: "After beginning daily methadone medication, she never used opiates again. After several attempts, she also achieved abstinence from Xanax, and remained completely drug free from that point forward."},
-      {name: "Kiley", image: "women/42.jpg", text: "After two months receiving methadone, Kiley became completely free of illicit drugs. Everything about her changed dramatically. She gained weight. Her depression lifted, and her mental clarity so much improved that it was like meeting a new person. Hidden under years of addiction-related exhaustion was a beautiful & capable human being with enormous ability and potential."},
-      {name: "Franklin", image: "men/30.jpg", text: "When Franklin began methadone maintenance, he experienced the kind of relief he had been hoping for. Withdrawal sickness ended. Like many people, his energy returned. He began to get a grip on his life, and a new focus. Franklin applied to a major university, and was accepted."},
-      {name: "Steven", image: "men/8.jpg", text: "He had heard about methadone and decided he should give it a chance since everything else he had tried did not work. The decision paid off. Methadone provided excellent relief, and enabled Steven to disconnect from the drugs that had been a part of his life for over a decade. Today, he is full time employed and primary caregiver for a child that enjoys a dedicated, loving, and stable father. Counseling, support, and methadone medication made a big difference in Steven's life. And the benefits extended to far more than just himself."},
-      {name: "Barry", image: "men/50.jpg", text: "Fast forward 6 years, Barry worked successfully through all phases in his methadone treatment clinic. Along the way, he maintained steady employment, joined a church, resumed parenting of his child, and achieved take home medication status. And then he tapered completely off of methadone."}
+      {name: "Sharon", image: "women/10.jpg", text: "After beginning daily methadone medication, she never used opiates again. After several attempts, she also achieved abstinence from Xanax, and remained completely drug free from that point forward..."},
+      {name: "Kiley", image: "women/42.jpg", text: "After two months receiving methadone, Kiley became completely free of illicit drugs. Everything about her changed dramatically. She gained weight. Her depression lifted, and her mental clarity so much improved that it was like meeting a new person. Hidden under years of addiction-related exhaustion was a beautiful & capable human being with enormous ability and potential..."},
+      {name: "Franklin", image: "men/30.jpg", text: "When Franklin began methadone maintenance, he experienced the kind of relief he had been hoping for. Withdrawal sickness ended. Like many people, his energy returned. He began to get a grip on his life, and a new focus. Franklin applied to a major university, and was accepted..."},
+      {name: "Steven", image: "men/8.jpg", text: "He had heard about methadone and decided he should give it a chance since everything else he had tried did not work. The decision paid off. Methadone provided excellent relief, and enabled Steven to disconnect from the drugs that had been a part of his life for over a decade. Today, he is full time employed and primary caregiver for a child that enjoys a dedicated, loving, and stable father. Counseling, support, and methadone medication made a big difference in Steven's life. And the benefits extended to far more than just himself..."},
+      {name: "Barry", image: "men/50.jpg", text: "Fast forward 6 years, Barry worked successfully through all phases in his methadone treatment clinic. Along the way, he maintained steady employment, joined a church, resumed parenting of his child, and achieved take home medication status. And then he tapered completely off of methadone..."}
     ]
   },
   buprenorphine : {
@@ -36,11 +36,11 @@ var info = {
     ],
     successStoriesLink: "http://www.naabt.org/true_stories.cfm",
     successStories: [
-      {name: "Russell", image: "men/13.jpg", text: "Not only has Suboxone given me another chance at life, it has given my kids their daddy back!"},
-      {name: "Victoria", image: "women/29.jpg", text: "My only regret with Suboxone is that I didn't try it sooner. My advice to anyone that is struggling with an addiction is to give it a try. I also believe that therapy is a very important part of recovery."},
-      {name: "Gloria", image: "women/8.jpg", text: "...It helped me a lot, but without the Suboxone I don't think I would have made it."},
+      {name: "Russell", image: "men/13.jpg", text: "Not only has Suboxone given me another chance at life, it has given my kids their daddy back!.."},
+      {name: "Victoria", image: "women/29.jpg", text: "My only regret with Suboxone is that I didn't try it sooner. My advice to anyone that is struggling with an addiction is to give it a try. I also believe that therapy is a very important part of recovery..."},
+      {name: "Gloria", image: "women/8.jpg", text: "...It helped me a lot, but without the Suboxone I don't think I would have made it..."},
       {name: "Keith", image: "men/33.jpg", text: "I would say if you are addicted to opiates to please give Suboxone a try – if you have tried every other way and failed first. Bupe isn't for everyone but it works wonders for me..."},
-      {name: "Ian", image: "men/27.jpg", text: "I am approaching 3 months on Suboxone, and I have my life back."},
+      {name: "Ian", image: "men/27.jpg", text: "I am approaching 3 months on Suboxone, and I have my life back..."},
     ]
   },
   naltrexone : {
@@ -56,13 +56,13 @@ var info = {
     ],
     successStoriesLink: "http://www.recoverystories.info/category/stories/",
     successStories: [
-      {name: "Adam", image: "men/44.jpg", text: "Naltrexone is not some sort of miracle cure. It is one thing that can help a person along their path to recovery. In my case, it helped give me some headspace (free of cravings) where I could focus on trying to learn to live without drugs and alcohol. The drug didn’t make me better. I made myself better, with the help of key people around me."},
+      {name: "Adam", image: "men/44.jpg", text: "Naltrexone is not some sort of miracle cure. It is one thing that can help a person along their path to recovery. In my case, it helped give me some headspace (free of cravings) where I could focus on trying to learn to live without drugs and alcohol. The drug didn’t make me better. I made myself better, with the help of key people around me..."},
     ]
   }
 };
 
 var loadGoodRXWidget = function(drug){
-  $("#goodrx_low-price_widget").empty()
+  // $("#goodrx_low-price_widget").empty()
   _grxdn = drug;
   (function(d, t) {
     var g = d.createElement(t),
